6a54cd17868e9a845c3251f2abe7052b282b6d94
absolute0kelvin <kelvin.wong@layerzerolabs.org>
2025-07-08 15:31:32 +0000

    add virtual sequences

--- tracer/src/instruction/amomaxud.rs
+++ tracer/src/instruction/amomaxud.rs
@@ -1,11 +1,27 @@
 use serde::{Deserialize, Serialize};
 
+use super::sltu::SLTU;
+use super::mul::MUL;
+use super::xori::XORI;
+use super::add::ADD;
+use super::ld::LD;
+use super::sd::SD;
+use super::format::format_i::FormatI;
+use super::format::format_load::FormatLoad;
+use super::format::format_s::FormatS;
+use super::format::format_virtual_halfword_alignment::HalfwordAlignFormat;
+use super::virtual_move::VirtualMove;
+use super::RAMWrite;
+use super::RV32IMInstruction;
+use super::VirtualInstructionSequence;
+use common::constants::virtual_register_index;
 use crate::{declare_riscv_instr, emulator::cpu::Cpu};
 
 use super::{
     format::{format_r::FormatR, InstructionFormat},
-    RISCVInstruction, RISCVTrace, RAMAtomic,
+    RISCVInstruction, RISCVTrace, RAMAtomic,RV32IMCycle,
 };
+
 
 declare_riscv_instr!(
     name   = AMOMAXUD,
@@ -51,4 +67,114 @@ impl AMOMAXUD {
     }
 }
 
-impl RISCVTrace for AMOMAXUD {} 
+impl RISCVTrace for AMOMAXUD {
+    fn trace(&self, cpu: &mut Cpu, trace: Option<&mut Vec<RV32IMCycle>>) {
+        let virtual_sequence = self.virtual_sequence(cpu);
+        let mut trace = trace;
+        for instr in virtual_sequence {
+            // In each iteration, create a new Option containing a re-borrowed reference
+            instr.trace(cpu, trace.as_deref_mut());
+        }
+    }
+}
+
+impl VirtualInstructionSequence for AMOMAXUD {
+    fn virtual_sequence(&self, cpu: &Cpu) -> Vec<RV32IMInstruction> {
+        let v_rs2 = virtual_register_index(6) as usize;
+        let v_rd = virtual_register_index(7) as usize;
+        let v_sel_rs2 = virtual_register_index(8) as usize;
+        let v_sel_rd = virtual_register_index(9) as usize;
+        let v_tmp = virtual_register_index(10) as usize;
+        let mut sequence = vec![];
+
+        let ld = LD {
+            address: self.address,
+            operands: FormatI {
+                rd: v_rd,
+                rs1: self.operands.rs1,
+                imm: 0,
+            },
+            virtual_sequence_remaining: Some(6),
+        };
+        sequence.push(ld.into());
+
+        let sltu = SLTU {
+            address: self.address,
+            operands: FormatR {
+                rd: v_sel_rs2,
+                rs1: v_rd,
+                rs2: self.operands.rs2,
+            },
+            virtual_sequence_remaining: Some(5),
+        };
+        sequence.push(sltu.into());
+
+        let xori = XORI {
+            address: self.address,
+            operands: FormatI {
+                rd: v_sel_rd,
+                rs1: v_sel_rs2,
+                imm: 1,
+            },
+            virtual_sequence_remaining: Some(4),
+        };
+        sequence.push(xori.into());
+
+        let mul = MUL {
+            address: self.address,
+            operands: FormatR {
+                rd: v_rs2,
+                rs1: v_sel_rs2,
+                rs2: self.operands.rs2,
+            },
+            virtual_sequence_remaining: Some(3),
+        };
+        sequence.push(mul.into());
+
+        let mul = MUL {
+            address: self.address,
+            operands: FormatR {
+                rd: v_tmp,
+                rs1: v_sel_rd,
+                rs2: v_rd,
+            },
+            virtual_sequence_remaining: Some(2),
+        };
+        sequence.push(mul.into());
+
+        let add = ADD {
+            address: self.address,
+            operands: FormatR {
+                rd: v_rs2,
+                rs1: v_tmp,
+                rs2: v_rs2,
+            },
+            virtual_sequence_remaining: Some(1),
+        };
+        sequence.push(add.into());
+
+        let sd = SD {
+            address: self.address,
+            operands: FormatS {
+                rs1: self.operands.rs1,
+                rs2: v_rs2,
+                imm: 0,
+            },
+            virtual_sequence_remaining: Some(1),
+        };
+        sequence.push(sd.into());
+
+        let vmove = VirtualMove {
+            address: self.address,
+            operands: FormatI {
+                rd: self.operands.rd,
+                rs1: v_rd,
+                imm: 0,
+            },
+            virtual_sequence_remaining: Some(0),
+        };
+        sequence.push(vmove.into());
+
+        sequence
+    }
+}
