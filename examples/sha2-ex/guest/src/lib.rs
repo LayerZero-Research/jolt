5768344643e438b95294a07eda350e3f55ed1ce6
Omid Bodaghi <omid.bodaghi@Omids-MacBook-Pro.local>
2025-07-09 16:57:07 -0700

    Add benchmark for program trace

--- examples/sha2-ex/guest/src/lib.rs
+++ examples/sha2-ex/guest/src/lib.rs
@@ -1,23 +1,9 @@
 #![cfg_attr(feature = "guest", no_std)]
 
-#[cfg(feature = "sha2_crate")]
-use sha2::{Sha256, Digest};
+use sha2::Digest;
 
 #[jolt::provable]
 fn sha2(input: &[u8]) -> [u8; 32] {
-    #[cfg(feature = "jolt_sha256")]
-    {
-        assert_eq!(1, 2);
-        // Use Jolt's optimized SHA256 implementation
-        jolt::sha256::Sha256::digest(input)
-    }
-    
-    #[cfg(feature = "sha2_crate")]
-    {
-        assert_eq!(1, 1);
-        // Use standard sha2 crate
-        let mut hasher = Sha256::new();
-        hasher.update(input);
-        hasher.finalize().into()
-    }
+    // Use Jolt's optimized SHA256 implementation
+    sha2::Sha256::digest(input).into()
 }
